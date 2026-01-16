ff001fa5bcbda626b00814bfc75b49630002694c
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-16 16:38:07 +0100

    feat: method execute added in HitStrategy interface

--- src/main/java/it/unibo/jnavy/model/captains/HitStrategy.java
+++ src/main/java/it/unibo/jnavy/model/captains/HitStrategy.java
@@ -1,5 +1,11 @@
 package it.unibo.jnavy.model.captains;
 
+import it.unibo.jnavy.model.ShotResult;
+import it.unibo.jnavy.model.grid.Grid;
+import it.unibo.jnavy.model.utilities.Position;
+
+import java.util.List;
+
 /**
  * Defines the strategy for executing a shot on the game grid.
  * This interface applies the Strategy Pattern to decouple the "shooter" (Captain)
@@ -9,4 +15,13 @@ package it.unibo.jnavy.model.captains;
  */
 public interface HitStrategy {
 
+    /**
+     * Executes the shot logic on the provided grid.
+     *
+     * @param target The central coordinate aimed by the player.
+     * @param grid The grid where the shot effects are applied.
+     * @return A list of {@link ShotResult}, representing the outcome for each cell affected.
+     * (e.g., a standard shot returns a list of size 1, an area shot returns multiple results).
+     */
+    List<ShotResult> execute(Position target, Grid grid);
 }
