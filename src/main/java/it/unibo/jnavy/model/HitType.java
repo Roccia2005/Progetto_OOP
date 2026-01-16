46b4757672f5cd6c7cfbcffeb87b5abc8a0f8437
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-16 08:46:56 +0100

    feat: HitType enum implemented

--- src/main/java/it/unibo/jnavy/model/HitType.java
+++ src/main/java/it/unibo/jnavy/model/HitType.java
@@ -1,4 +1,25 @@
 package it.unibo.jnavy.model;
 
 public enum HitType {
+
+    MISS("Missed, just water!"),
+    HIT("Target hit!"),
+    SUNK("Ship sunk"),
+    ALREADY_HIT("You already fired here!"),
+    INVALID("Invalid coordinates");
+
+    private final String description;
+
+    HitType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean isSuccessful() {
+        return this == HIT || this == SUNK;
+    }
+    
 }
