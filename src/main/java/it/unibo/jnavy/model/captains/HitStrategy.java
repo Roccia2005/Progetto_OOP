3e501edb6100c0bd53df3b4149fef8283b7e8bc3
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-16 15:33:48 +0100

    fix: new comments in HitStrategy

--- src/main/java/it/unibo/jnavy/model/captains/HitStrategy.java
+++ src/main/java/it/unibo/jnavy/model/captains/HitStrategy.java
@@ -1,5 +1,12 @@
 package it.unibo.jnavy.model.captains;
 
+/**
+ * Defines the strategy for executing a shot on the game grid.
+ * This interface applies the Strategy Pattern to decouple the "shooter" (Captain)
+ * from the "ballistics" (how the shot affects the grid).
+ *
+ * Different implementations can define unique firing patterns.
+ */
 public interface HitStrategy {
 
 }
