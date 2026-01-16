8c3f8c95cd111082d8b204bee248954ae0c637a8
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-16 15:41:56 +0100

    feat: method miss implemented

--- src/main/java/it/unibo/jnavy/model/ShotResult.java
+++ src/main/java/it/unibo/jnavy/model/ShotResult.java
@@ -1,5 +1,10 @@
 package it.unibo.jnavy.model;
 
+import it.unibo.jnavy.model.ship.Ship;
+import it.unibo.jnavy.model.utilities.Position;
+
+import java.util.Optional;
+
 /**
  * Represents the immutable outcome of a shot fired at the grid.
  * It encapsulates all necessary information about the attack:
@@ -7,5 +12,15 @@ package it.unibo.jnavy.model;
  *   - The exact position where the event occurred
  *   - The specific ship involved (if sunk)
  */
-public record ShotResult() {
+public record ShotResult(HitType hitType, Position position, Optional<Ship> sunkShip) {
+
+    /**
+     * Creates a result indicating a missed shot.
+     *
+     * @param position The position where the shot was fired.
+     * @return A ShotResult with a MISS hit type.
+     */
+    public static ShotResult miss(final Position position) {
+        return new ShotResult(HitType.MISS, position, Optional.empty());
+    }
 }
