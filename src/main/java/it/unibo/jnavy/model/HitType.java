1158d1ce6aa844c4fe08404e1ae7c57700b3b1bf
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-16 12:09:49 +0100

    fix: comments in HitType

--- src/main/java/it/unibo/jnavy/model/HitType.java
+++ src/main/java/it/unibo/jnavy/model/HitType.java
@@ -1,25 +1,63 @@
 package it.unibo.jnavy.model;
 
+/**
+ * Represents the possible outcomes of a shot fired at the game grid.
+ * This enum is used to communicate the results of an attack.
+ */
 public enum HitType {
 
+    /**
+     * The shot landed in water; no ship was hit.
+     */
     MISS("Missed, just water!"),
+
+    /**
+     * A ship was hit. but is still afloat.
+     */
     HIT("Target hit!"),
+
+    /**
+     * A ship was hit and sunk.
+     */
     SUNK("Ship sunk"),
+
+    /**
+     * The player tried to shoot at a coordinate that was already hit in a previous turn.
+     */
     ALREADY_HIT("You already fired here!"),
+
+    /**
+     * The target coordinates are outside the grid boundaries or are invalid.
+     */
     INVALID("Invalid coordinates");
 
     private final String description;
 
+    /**
+     * Constructor for the HitType enum.
+     *
+     * @param description A human-readable description of the shot result.
+     */
     HitType(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns the human-readable description of the result.
+     *
+     * @return The description string.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Checks if the shot was successful (i.e., it hit or sunk a ship).
+     *
+     * @return true if the result is HIT or SUNK, false otherwise.
+     */
     public boolean isSuccessful() {
         return this == HIT || this == SUNK;
     }
-    
+
 }
