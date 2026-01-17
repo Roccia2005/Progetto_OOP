7882bcadcd1f8f231cbd2665d39386709651e371
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-01-17 17:03:51 +0100

    player interface sketch

--- src/main/java/it/unibo/jnavy/model/Player.java
+++ src/main/java/it/unibo/jnavy/model/Player.java
@@ -1,5 +1,16 @@
 package it.unibo.jnavy.model;
 
-public interface Player {
+import java.util.List;
 
+import it.unibo.jnavy.model.fleet.Fleet;
+import it.unibo.jnavy.model.ship.Ship;
+
+public interface Player {
+    /*
+        metodo per ottenere la flotta (lista di navi) = List<Ship> getShips();
+
+        metodo per creare uno sparo (o StandardShot o AreaShot) che conterrà una position, che verrà poi inviato a weather;
+
+
+    */
 }
