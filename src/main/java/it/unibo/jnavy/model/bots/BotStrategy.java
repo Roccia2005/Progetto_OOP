8c32197b7811fedad2b9460514be14edc90a1663
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-01-18 22:48:04 +0100

    start implementation BotStrategies

--- src/main/java/it/unibo/jnavy/model/bots/BotStrategy.java
+++ src/main/java/it/unibo/jnavy/model/bots/BotStrategy.java
@@ -1,5 +1,14 @@
 package it.unibo.jnavy.model.bots;
 
+/**
+ * deve definire il contratto principale a cui si devono attenere le classi delle 3 strategie
+ */
 public interface BotStrategy {
+    /*
 
+        metodo [ Position selectTarget(enemyGrid) ] da aggiungere perchè in Bot.java lo uso in public decideTarget
+
+
+
+    */
 }
