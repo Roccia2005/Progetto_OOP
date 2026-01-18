7f2876e2fb56e04ae77b40d60d199c8958ac71e0
Samuele <samuele.rocca2@studio.unibo.it>
2026-01-18 15:25:45 +0100

    implement class Gunner

--- src/main/java/it/unibo/jnavy/model/captains/Gunner.java
+++ src/main/java/it/unibo/jnavy/model/captains/Gunner.java
@@ -1,5 +1,25 @@
 package it.unibo.jnavy.model.captains;
 
-public class Gunner implements Captain{
+import it.unibo.jnavy.model.grid.Grid;
+import it.unibo.jnavy.model.utilities.Position;
+
+public class Gunner extends AbstractCaptain{
+
+    private static final int COOLDOWN = 3;
+
+    public Gunner() {
+        super(COOLDOWN);
+    }
+
+    @Override
+    public boolean useAbility(Grid grid, Position p) {
+        if (this.isAbilityRecharged()) {
+            HitStrategy areaShot = new AreaShot();
+            areaShot.execute(p, grid);
+            this.resetCooldown();
+            return true;
+        }
+        return false;
+    }
     
 }
