de52d049742dae053f803d4c2ef23116c8c6335d
Samuele <samuele.rocca2@studio.unibo.it>
2026-01-18 15:25:25 +0100

    implement class SonarOfficer

--- src/main/java/it/unibo/jnavy/model/captains/SonarOfficer.java
+++ src/main/java/it/unibo/jnavy/model/captains/SonarOfficer.java
@@ -1,5 +1,28 @@
 package it.unibo.jnavy.model.captains;
 
-public class SonarOfficer implements Captain{
-    
+import java.util.Optional;
+
+import it.unibo.jnavy.model.cell.Cell;
+import it.unibo.jnavy.model.grid.Grid;
+import it.unibo.jnavy.model.utilities.Position;
+
+public class SonarOfficer extends AbstractCaptain {
+
+    private static final int COOLDOWN = 3;
+
+    public SonarOfficer() {
+        super(COOLDOWN);
+    }
+
+    @Override
+    public boolean useAbility(Grid grid, Position p) {
+        Optional<Cell> cell = grid.getCell(p);
+        if (cell.isPresent()) {
+            cell.get().setVisible();
+            this.resetCooldown();
+            return true;
+        }
+        return false;
+    }
+
 }
