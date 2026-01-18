534ab1a2dfe4a804016cd8b85e3a356763c78370
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-18 18:16:06 +0100

    feat: constructor in class WeatherManagerImpl

--- src/main/java/it/unibo/jnavy/model/weather/WeatherManagerImpl.java
+++ src/main/java/it/unibo/jnavy/model/weather/WeatherManagerImpl.java
@@ -1,5 +1,7 @@
 package it.unibo.jnavy.model.weather;
 
+import java.util.Random;
+
 /**
  * Concrete implementation of the {@link WeatherManager}.
  * This class handles the logic for random weather transitions based on turn counters.
@@ -9,4 +11,15 @@ package it.unibo.jnavy.model.weather;
  *    - Whether a shot misses due to bad weather conditions.
  */
 public class WeatherManagerImpl {
+
+    private static final int WEATHER_DURATION = 5;
+    private WeatherCondition condition;
+    private int turnCounter;
+    private Random random;
+
+    WeatherManagerImpl() {
+        this.condition = WeatherCondition.SUNNY;
+        this.turnCounter = 0;
+        this.random = new Random();
+    }
 }
