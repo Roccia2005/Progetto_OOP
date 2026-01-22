47052a8b292a8fd516d600f83122f659e810f5f2
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-01-22 12:20:39 +0100

    fixed BeginnerBot class and add exception

--- src/main/java/it/unibo/jnavy/model/bots/BeginnerBot.java
+++ src/main/java/it/unibo/jnavy/model/bots/BeginnerBot.java
@@ -6,23 +6,37 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+import it.unibo.jnavy.model.cell.Cell;
 import it.unibo.jnavy.model.grid.Grid;
 import it.unibo.jnavy.model.utilities.Position;
 
 public class BeginnerBot implements BotStrategy{
 
+    private final Random random = new Random();
+
     @Override
     public Position selectTarget(Grid enemyGrid) {
         List<Position> cellsList = getCellsList(enemyGrid);
 
-        Random random = new Random();
-        int randomIndex = random.nextInt(cellsList.size());
+        if (cellsList.isEmpty()) {
+            throw new IllegalStateException("The bot can't shoot, no valid cells in grid");
+        }
 
+        int randomIndex = getRandomIndex(cellsList);
         return cellsList.get(randomIndex);
     }
 
     public List<Position> getCellsList(Grid grid) {
-        List<Position> enemyGridToList = Arrays.stream(grid.getCellsMatrix()).flatMap(Arrays::stream).map(c -> c.getPosition()).collect(Collectors.toList());
+        List<Position> enemyGridToList = Arrays.stream(grid.getCellsMatrix())
+        .flatMap(Arrays::stream)
+        .filter(c -> !c.isHit())
+        .map(Cell::getPosition)
+        .collect(Collectors.toList());
         return enemyGridToList;
+    }
+
+    public int getRandomIndex(final List<Position> cellsList) {
+        int index = random.nextInt(cellsList.size());
+        return index;
     }
 }
