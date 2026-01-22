983783adcf89fb6b7ddb6fe1d61cd361522cdd71
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-01-22 15:13:30 +0100

    implemented AbstractBotStrategy class and refined BeginnerBot class

---
+++ src/main/java/it/unibo/jnavy/model/bots/AbstractBotStrategy.java
@@ -0,0 +1,39 @@
+package it.unibo.jnavy.model.bots;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import it.unibo.jnavy.model.cell.Cell;
+import it.unibo.jnavy.model.grid.Grid;
+import it.unibo.jnavy.model.utilities.Position;
+
+public abstract class AbstractBotStrategy implements BotStrategy{
+    protected final Random random = new Random();
+
+    protected Position getRandomValidPosition(Grid enemyGrid) {
+        List<Position> cellsList = getCellsList(enemyGrid);
+
+        if (cellsList.isEmpty()) {
+            throw new IllegalStateException("The bot can't shoot, no valid cells in grid");
+        }
+        int randomIndex = getRandomIndex(cellsList);
+
+        return cellsList.get(randomIndex);
+    }
+
+    protected List<Position> getValidCellsList(Grid grid) {
+        List<Position> enemyGridToList = Arrays.stream(grid.getCellsMatrix())
+        .flatMap(Arrays::stream)
+        .filter(c -> !c.isHit())
+        .map(Cell::getPosition)
+        .collect(Collectors.toList());
+        return enemyGridToList;
+    }
+
+    protected int getRandomIndex(final List<Position> cellsList) {
+        int index = random.nextInt(cellsList.size());
+        return index;
+    }
+}
--- src/main/java/it/unibo/jnavy/model/bots/BeginnerBot.java
+++ src/main/java/it/unibo/jnavy/model/bots/BeginnerBot.java
@@ -1,42 +1,12 @@
 package it.unibo.jnavy.model.bots;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.stream.Collectors;
-
-import it.unibo.jnavy.model.cell.Cell;
 import it.unibo.jnavy.model.grid.Grid;
 import it.unibo.jnavy.model.utilities.Position;
 
-public class BeginnerBot implements BotStrategy{
-
-    private final Random random = new Random();
+public class BeginnerBot extends AbstractBotStrategy{
 
     @Override
     public Position selectTarget(Grid enemyGrid) {
-        List<Position> cellsList = getCellsList(enemyGrid);
-
-        if (cellsList.isEmpty()) {
-            throw new IllegalStateException("The bot can't shoot, no valid cells in grid");
-        }
-        int randomIndex = getRandomIndex(cellsList);
-
-        return cellsList.get(randomIndex);
-    }
-
-    public List<Position> getCellsList(Grid grid) {
-        List<Position> enemyGridToList = Arrays.stream(grid.getCellsMatrix())
-        .flatMap(Arrays::stream)
-        .filter(c -> !c.isHit())
-        .map(Cell::getPosition)
-        .collect(Collectors.toList());
-        return enemyGridToList;
-    }
-
-    public int getRandomIndex(final List<Position> cellsList) {
-        int index = random.nextInt(cellsList.size());
-        return index;
+        return super.getRandomValidPosition(enemyGrid);
     }
 }
