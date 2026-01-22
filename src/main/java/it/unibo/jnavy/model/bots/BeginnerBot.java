64da0dfbf5fda32be768c66aefa57d57c82a9e16
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-01-22 11:50:51 +0100

    implemented BeginnerBot

--- src/main/java/it/unibo/jnavy/model/bots/BeginnerBot.java
+++ src/main/java/it/unibo/jnavy/model/bots/BeginnerBot.java
@@ -1,5 +1,11 @@
 package it.unibo.jnavy.model.bots;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 import it.unibo.jnavy.model.grid.Grid;
 import it.unibo.jnavy.model.utilities.Position;
 
@@ -7,8 +13,16 @@ public class BeginnerBot implements BotStrategy{
 
     @Override
     public Position selectTarget(Grid enemyGrid) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'selectTarget'");
+        List<Position> cellsList = getCellsList(enemyGrid);
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(cellsList.size());
+
+        return cellsList.get(randomIndex);
     }
 
+    public List<Position> getCellsList(Grid grid) {
+        List<Position> enemyGridToList = Arrays.stream(grid.getCellsMatrix()).flatMap(Arrays::stream).map(c -> c.getPosition()).collect(Collectors.toList());
+        return enemyGridToList;
+    }
 }
