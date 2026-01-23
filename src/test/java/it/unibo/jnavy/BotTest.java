738098a1a73c9081af9652667aa250c6aeb914d1
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-01-23 11:42:35 +0100

    fixed errors in bottest

--- src/test/java/it/unibo/jnavy/BotTest.java
+++ src/test/java/it/unibo/jnavy/BotTest.java
@@ -1,15 +1,12 @@
 package it.unibo.jnavy;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.beans.Transient;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import model.bots.*;
 
-public class TestProva {
+public class BotTest {
 
     @Test
     void createBeginnerBot() {
