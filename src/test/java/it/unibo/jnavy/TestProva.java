657820524e6a48b65cda4ecbb1285c4d18a70c5e
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-01-23 11:36:01 +0100

    create bot test class

--- src/test/java/it/unibo/jnavy/TestProva.java
+++ src/test/java/it/unibo/jnavy/TestProva.java
@@ -1,4 +1,18 @@
 package it.unibo.jnavy;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.beans.Transient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import model.bots.*;
+
 public class TestProva {
+
+    @Test
+    void createBeginnerBot() {
+
+    }
 }
