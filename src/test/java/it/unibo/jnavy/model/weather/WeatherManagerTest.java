7377c162d60ead0f1560057fa6238b4c26cc2ae0
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-01-23 12:05:50 +0100

    feat: setUp method for test

--- src/test/java/it/unibo/jnavy/model/weather/WeatherManagerTest.java
+++ src/test/java/it/unibo/jnavy/model/weather/WeatherManagerTest.java
@@ -1,4 +1,21 @@
 package it.unibo.jnavy.model.weather;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+import it.unibo.jnavy.model.grid.GridImpl;
+import it.unibo.jnavy.model.utilities.Position;
+
+/**
+ * Test class for {@link WeatherManagerImpl}.
+ */
 public class WeatherManagerTest {
+
+    private WeatherManager weatherManager;
+
+    @BeforeEach
+    void setUp() {
+        this.weatherManager = new WeatherManagerImpl();
+    }
 }
