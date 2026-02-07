241f4faf9c7838a31b18f130a87344c79dd9623d
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-02-07 21:50:23 +0100

    started implementing tests for bot classes

---
+++ src/test/java/it/unibo/jnavy/model/bots/BotTest.java
@@ -0,0 +1,66 @@
+package it.unibo.jnavy;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import it.unibo.jnavy.model.grid.GridImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import model.bots.*;
+
+class BotTest {
+
+    private Grid grid;
+
+    @BeforeEach
+    void setUp() {
+        this.grid = new GridImpl();
+    }
+
+    @Test
+    void testBeginnerBot() {
+
+    }
+
+    @Test
+    void testProBotHunting() {
+
+    }
+
+    @Test
+    void testProBotSeeking() {
+
+    }
+
+    @Test
+    void testProBotDestroying() {
+
+    }
+
+    @Test
+    void testProBotReturnToHuntingAfterSunk() {
+
+    }
+
+    @Test
+    void testProBotReverseDirection() {
+
+    }
+
+    @Test
+    void testSniperBotHalfGridPriority() {
+
+    }
+
+    @Test
+    void testSniperBotSwitchOnRight() {
+
+    }
+
+    @Test
+    void testSniperBotBorderCrossing() {
+
+    }
+
+}
