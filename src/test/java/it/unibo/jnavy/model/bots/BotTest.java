eea0499565d9dbf503afbfbaa62a33536ed20832
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-02-09 17:34:21 +0100

    implemented beginnerbot test method

--- src/test/java/it/unibo/jnavy/model/bots/BotTest.java
+++ src/test/java/it/unibo/jnavy/model/bots/BotTest.java
@@ -2,25 +2,39 @@ package it.unibo.jnavy;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import it.unibo.jnavy.model.grid.GridImpl;
+import it.unibo.jnavy.model.bots.BeginnerBot;
+import it.unibo.jnavy.model.bots.BotStrategy;
+import it.unibo.jnavy.model.utilities.Position;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import model.bots.*;
+import model.*;
 
+/**
+ * Test class for {@link BotStrategy}
+ * This class verifies the behavior of the different bot strategies.
+ * It covers BeginnerBot, ProBot and SniperBot logics.
+ */
 class BotTest {
 
     private Grid grid;
+    private static final int G_SIZE = 10;
 
     @BeforeEach
     void setUp() {
         this.grid = new GridImpl();
     }
 
+    /**
+     * Test for {@link BeginnerBot}.
+     * Verifies that the bot generates random valid positions and never shoots at the same.
+     */
     @Test
     void testBeginnerBot() {
-
+        BotStrategy bot = new BeginnerBot();
+        Position target = bot.selectTarget(this.grid);
+        AssertTrue(validPosition(target));
     }
 
     @Test
@@ -63,4 +77,10 @@ class BotTest {
 
     }
 
+    private boolean validPosition(Position p) {
+        return p.x() >= 0
+        && p.x() < G_SIZE
+        && p.y() >= 0
+        && p.y() < G_SIZE;
+    }
 }
