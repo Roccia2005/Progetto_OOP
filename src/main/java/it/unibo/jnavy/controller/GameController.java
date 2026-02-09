e03d93c433e1ae18133ffac0180d5bc55e41fe26
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-02-09 22:33:30 +0100

    add few methods in controller interface

--- src/main/java/it/unibo/jnavy/controller/GameController.java
+++ src/main/java/it/unibo/jnavy/controller/GameController.java
@@ -1,6 +1,9 @@
 package it.unibo.jnavy.controller;
 
+import it.unibo.jnavy.model.utilities.Position;
+
 public interface GameController {
 
-    
+    void startGame();
+    void processShot(Position p);
 }
