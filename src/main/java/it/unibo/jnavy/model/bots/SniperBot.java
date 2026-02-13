18d8f8f28b9dfac7822ff1c94db833f32c246f45
Riccardo <riccardo.antonellini@studio.unibo.it>
2026-02-13 17:17:30 +0100

    add override in lastShotFeedback of sniperbot to avoid state pollution with probot

--- src/main/java/it/unibo/jnavy/model/bots/SniperBot.java
+++ src/main/java/it/unibo/jnavy/model/bots/SniperBot.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
+import it.unibo.jnavy.model.HitType;
 import it.unibo.jnavy.model.cell.Cell;
 import it.unibo.jnavy.model.grid.Grid;
 import it.unibo.jnavy.model.ship.Ship;
@@ -126,21 +127,12 @@ public class SniperBot extends ProBot{
         return halfValidCellsDx.get(super.getRandomIndex(halfValidCellsDx));
     }
 
-    /**
-     * Cosa viene fornito allo sniper bot:
-     * - enemyGrid = griglia avversaria reale (il controller passa direttamente al costruttore dello sniper la human.getGrid() alla creazione di esso)
-     * - lo sniper si prende la metà sinistra e salva tutto in una lista knownTargets
-     *
-     * Cosa fa lo sniper bot finchè ci sono celle in knownTargets:
-     * - come prima cella da colpire sceglie la prima cella occupata che incontra nella lista knownTargets;
-     * - se colpisce una nave (HIT) ma non la affonda, controlla la knownTargets:
-     *      1. se il resto della nave è ancora nella lista (quindi nella metà sinistra): continua a sparare a quelle coordinate.
-     *      2. se il resto della nave NON è nella lista (quindi la nave attraversa il confine verso destra):
-     *              a. il Bot calcola la prossima cella adiacente verso DESTRA (nella zona buia) e spara li'.
-     *
-     * Cosa fa lo sniper bot quando knownTargets è vuota:
-     * - distrutte tutte le navi nella metà sinistra, sniperBot si comporta come proBot MA solo nella metà destra.
-     *
-     * Devo ricordarmi di estendere da ProBot direttamente così da avere il comportamento già pronto!!!
-     */
+    @Override
+    public void lastShotFeedback(final Position p, final HitType state) {
+        // ho ancora knownTargets a sinistra quindi evito di intaccare la logica di probot col rischio di colpire a sinistra in seeking
+        if (!this.knownTargets.isEmpty()) {
+            return;
+        }
+        super.lastShotFeedback(p, state);
+    }
 }
