d35b34d2e0525c9db77234344535cfa455d0a3e2
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-02-16 15:01:49 +0100

    feat: view architecture implemented

---
+++ src/main/java/it/unibo/jnavy/view/ViewGUI.java
@@ -0,0 +1,11 @@
+package it.unibo.jnavy.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ViewGUI extends JFrame implements View {
+
+    private final CardLayout cardLayout;
+    private final JPanel mainPanel;
+
+}
