4393943c0eff32e5d724a11639233b93cd365bfb
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-02-16 13:07:57 +0100

    fix: weather iconLabel shape

--- src/main/java/it/unibo/jnavy/view/components/WeatherWidget.java
+++ src/main/java/it/unibo/jnavy/view/components/WeatherWidget.java
@@ -21,16 +21,37 @@ public class WeatherWidget extends JPanel {
      * a large centered icon.
      */
     public WeatherWidget() {
-        this.setLayout(new BorderLayout());
-        this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        this.setLayout(new GridBagLayout());
+        this.setOpaque(false);
         this.setPreferredSize(new Dimension(80, 80));
 
         this.iconLabel = new JLabel("☀\uFE0F", SwingConstants.CENTER);
-        this.iconLabel.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 50));
+        this.iconLabel.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 40));
 
-        this.add(iconLabel, BorderLayout.CENTER);
+        this.add(iconLabel);
 
         updateWeather(WeatherCondition.SUNNY);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        int diameter = Math.min(getWidth(), getHeight()) - 4;
+        int x = (getWidth() - diameter) / 2;
+        int y = (getHeight() - diameter) / 2;
+
+        g2.setColor(getBackground());
+        g2.fillOval(x, y, diameter, diameter);
+
+        g2.setColor(Color.DARK_GRAY);
+        g2.setStroke(new BasicStroke(2));
+        g2.drawOval(x, y, diameter, diameter);
+
+        g2.dispose();
+
+        super.paintComponent(g);
     }
 
     /**
