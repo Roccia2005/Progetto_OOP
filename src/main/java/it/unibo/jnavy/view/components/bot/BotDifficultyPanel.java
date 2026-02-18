a3921714cb2b1be20a1a2994f63911370edfe04f
Samuele <samuele.rocca2@studio.unibo.it>
2026-02-18 20:37:17 +0100

    add new BotComponent

---
+++ src/main/java/it/unibo/jnavy/view/components/bot/BotDifficultyPanel.java
@@ -0,0 +1,23 @@
+package it.unibo.jnavy.view.components.bot;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+public class BotDifficultyPanel extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    public BotDifficultyPanel(final String difficultyName) {
+        super(new FlowLayout(FlowLayout.CENTER, 15, 0));
+        this.setOpaque(false);
+
+        final JLabel difficultyLabel = new JLabel("Difficulty: " + difficultyName);
+        difficultyLabel.setForeground(Color.BLACK);
+        difficultyLabel.setFont(new Font("Arial", Font.BOLD, 14));
+
+        this.add(difficultyLabel);
+    }
+}
