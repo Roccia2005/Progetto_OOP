8b29b5c552b50b9c2c41724054a2a715878125a8
Samuele <samuele.rocca2@studio.unibo.it>
2026-02-18 23:33:16 +0100

    create new components CaptainNamePanel

---
+++ src/main/java/it/unibo/jnavy/view/components/captain/CaptainNamePanel.java
@@ -0,0 +1,23 @@
+package it.unibo.jnavy.view.components.captain;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+public class CaptainNamePanel extends JPanel {
+    
+    private static final long serialVersionUID = 1L;
+
+    public CaptainNamePanel(String captainName) {
+        super(new FlowLayout(FlowLayout.CENTER, 15, 0));
+        this.setOpaque(false);
+
+        JLabel nameLabel = new JLabel("Captain: " + captainName);
+        nameLabel.setForeground(Color.BLACK);
+        nameLabel.setFont(new Font("SansSerif", Font.BOLD, 14));
+
+        this.add(nameLabel);
+    }
+}
