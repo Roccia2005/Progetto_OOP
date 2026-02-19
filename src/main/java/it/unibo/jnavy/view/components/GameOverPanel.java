fceca03987a5e8ad602e963c638fa7bd12a59680
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-02-19 06:35:56 +0100

    feat: paintCOmponent overrided

--- src/main/java/it/unibo/jnavy/view/components/GameOverPanel.java
+++ src/main/java/it/unibo/jnavy/view/components/GameOverPanel.java
@@ -58,6 +58,26 @@ public class GameOverPanel extends JPanel {
         setVisible(true);
     }
 
+    @Override
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(new Color(0, 0, 0, 200));
+        g2.fillRect(0, 0, getWidth(), getHeight());
+
+        Image imgToDraw = isWin ? winImage : loseImage;
+
+        if (imgToDraw != null) {
+            int targetW = 400;
+            int targetH = (int) ((double)imgToDraw.getHeight(null) / imgToDraw.getWidth(null) * targetW);
+
+            int x = (getWidth() - targetW) / 2;
+            int y = (getHeight() / 2) - targetH - 50;
+
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+            g2.drawImage(imgToDraw, x, y, targetW, targetH, this);
+        }
+    }
+
     private Image loadImage(final String filename) {
 
     }
