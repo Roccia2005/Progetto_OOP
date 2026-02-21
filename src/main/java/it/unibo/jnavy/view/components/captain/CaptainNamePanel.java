dfbe71f021e87f3fb72957200893a580ecb884f8
Enrico Solaroli <enrico.solaroli@studio,unibo,it>
2026-02-21 23:40:48 +0100

    fix checkstyle

--- src/main/java/it/unibo/jnavy/view/components/captain/CaptainNamePanel.java
+++ src/main/java/it/unibo/jnavy/view/components/captain/CaptainNamePanel.java
@@ -4,6 +4,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import static it.unibo.jnavy.view.utilities.ViewConstants.FONT_FAMILY;
+import static it.unibo.jnavy.view.utilities.ViewConstants.FONT_SIZE_CTRL;
 import static it.unibo.jnavy.view.utilities.ViewConstants.FOREGROUND_COLOR;
 
 import java.awt.FlowLayout;
@@ -15,22 +16,24 @@ import java.awt.Font;
  */
 public final class CaptainNamePanel extends JPanel {
 
+    private static final int H_GAP = 15;
+    private static final int V_GAP = 0;
+
     @java.io.Serial
     private static final long serialVersionUID = 1L;
 
     /**
      * Constructs a new {@code CaptainNamePanel} with the specified captain name.
      *
-     * @param captainName the string representing the name of 
-     * the selected captain to be displayed.
+     * @param captainName the string representing the name of the selected captain to be displayed.
      */
     public CaptainNamePanel(final String captainName) {
-        super(new FlowLayout(FlowLayout.CENTER, 15, 0));
+        super(new FlowLayout(FlowLayout.CENTER, H_GAP, V_GAP));
         this.setOpaque(false);
 
         final JLabel nameLabel = new JLabel("Captain: " + captainName);
         nameLabel.setForeground(FOREGROUND_COLOR);
-        nameLabel.setFont(new Font(FONT_FAMILY, Font.BOLD, 14));
+        nameLabel.setFont(new Font(FONT_FAMILY, Font.BOLD, FONT_SIZE_CTRL));
 
         this.add(nameLabel);
     }
