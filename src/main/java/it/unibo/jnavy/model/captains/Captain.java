1e74b264194642c662ab29ca86ec28e8b3f9b131
mattiabandini1 <mattia.bandini3@studio.unibo.it>
2026-02-21 21:15:43 +0100

    fix: checkstyle in Captain

--- src/main/java/it/unibo/jnavy/model/captains/Captain.java
+++ src/main/java/it/unibo/jnavy/model/captains/Captain.java
@@ -58,6 +58,7 @@ public interface Captain extends TurnObserver {
 
     /**
      * Returns the name of the captain.
+     * 
      * @return the simple class name representing the captain type.
      */
     String getName();
