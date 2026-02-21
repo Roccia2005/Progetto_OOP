b29f47da5d8473edca1b2fb80f3774530e9cfafb
Enrico Solaroli <enrico.solaroli@studio,unibo,it>
2026-02-21 23:37:15 +0100

    fix checkstyle

--- src/main/java/it/unibo/jnavy/view/components/grid/RowLabelsPanel.java
+++ src/main/java/it/unibo/jnavy/view/components/grid/RowLabelsPanel.java
@@ -1,6 +1,7 @@
 package it.unibo.jnavy.view.components.grid;
 
 import static it.unibo.jnavy.view.utilities.ViewConstants.FONT_FAMILY;
+import static it.unibo.jnavy.view.utilities.ViewConstants.FONT_SIZE_CTRL;
 import static it.unibo.jnavy.view.utilities.ViewConstants.FOREGROUND_COLOR;
 
 import java.awt.Font;
@@ -17,25 +18,26 @@ import javax.swing.SwingConstants;
  */
 public final class RowLabelsPanel extends JPanel {
 
+    private static final int PADDING_RIGHT = 5;
+
     @java.io.Serial
     private static final long serialVersionUID = 1L;
 
     /**
      * Constructs a new {@code RowLabelsPanel} generating alphabetical labels.
      *
-     * @param size the total number of rows in the grid, which determines 
-     * how many alphabetical labels are created.
+     * @param size the total number of rows in the grid, which determines how many alphabetical labels are created.
      */
     public RowLabelsPanel(final int size) {
-        super(new GridLayout(size, 1, 2, 2)); 
+        super(new GridLayout(size, 1, 2, 2));
         this.setOpaque(false);
-        this.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
+        this.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, PADDING_RIGHT));
 
         for (int i = 0; i < size; i++) {
             final char letter = (char) ('A' + i);
             final JLabel label = new JLabel(String.valueOf(letter), SwingConstants.CENTER);
             label.setForeground(FOREGROUND_COLOR);
-            label.setFont(new Font(FONT_FAMILY, Font.BOLD, 14));
+            label.setFont(new Font(FONT_FAMILY, Font.BOLD, FONT_SIZE_CTRL));
             this.add(label);
         }
     }
